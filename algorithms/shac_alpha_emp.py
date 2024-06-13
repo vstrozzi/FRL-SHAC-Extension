95cc912b632d99e161b6b65277fe216310b649d2
vstrozzi <vstrozzi@student.ethz.ch>
2024-06-13 14:57:40 +0200

    p

--- algorithms/shac_alpha_emp.py
+++ algorithms/shac_alpha_emp.py
@@ -296,14 +296,10 @@ class SHAC_ALPHA_EMP:
             
             # Perturbe the weight of the model with noise
             with torch.no_grad():
-                # Clone the actor
                 for _ in range(self.nr_query):
                     for lay, param, in zip(params, self.actor.parameters()):
                         # Add gaussian noise to parameters with fixed sigma
                         nr_params = torch.numel(params[lay])
-                        epsilon = torch.normal(0, 1, size=(1, nr_params)
-                                        ).squeeze(0).reshape(params[lay].shape)
-
 
                     """
                     # Eval 0th order gradient
