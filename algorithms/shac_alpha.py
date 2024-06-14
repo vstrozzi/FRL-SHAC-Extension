b09f0ef360f12cc8a79cd531f9fbbf2bbdb52a02
vstrozzi <vstrozzi@student.ethz.ch>
2024-06-14 14:55:15 +0200

    trying 0 order with smaller sigma

--- algorithms/shac_alpha.py
+++ algorithms/shac_alpha.py
@@ -114,7 +114,7 @@ class SHAC_ALPHA:
             self.steps_num = self.env.episode_length
 
         # IMPL: smoothing noise
-        self.sigma = 0.1 #cfg['params']['config'].get('sigma', 0.1)
+        self.sigma = 0.01 #cfg['params']['config'].get('sigma', 0.1)
         self.threshold_grad_norm_diff = 2
 
         # create actor critic network
