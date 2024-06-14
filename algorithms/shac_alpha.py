b5e80f4cb7d0c7b8b7ef0ee7f2aab9f33906cd8e
vstrozzi <vstrozzi@student.ethz.ch>
2024-06-14 11:47:03 +0200

    trying only 1 order

--- algorithms/shac_alpha.py
+++ algorithms/shac_alpha.py
@@ -505,13 +505,13 @@ class SHAC_ALPHA:
 
 
             # Eval the 1th order gradient per environment and then batch it
-            for env in range(self.num_envs):
+            """ for env in range(self.num_envs):
                 self.actor_optimizer.zero_grad()
                 # Detach graph with last backward
                 actor_loss_env[env].backward(retain_graph=True)
                 for lay in self.grad_1th_order.keys():   
                     self.grad_1th_order_env[lay][env] = params[lay].grad.clone().detach()
-                    self.grad_1th_order[lay] = self.grad_1th_order[lay] + self.grad_1th_order_env[lay][env]/self.num_envs
+                    self.grad_1th_order[lay] = self.grad_1th_order[lay] + self.grad_1th_order_env[lay][env]/self.num_envs """
             del params
 
             # Eval std of 1th order gradient and B (norm of difference of grad 1 and 0 estimate) to decide alpha gradient
@@ -548,8 +548,8 @@ class SHAC_ALPHA:
             print('grad_0th_iter:', self.grad_0th_order_std_scal)
             print('alpha_gamma_iter:', self.alpha_gamma)
             for param, lay in zip(self.actor.parameters(), dict(self.actor.named_parameters()).keys()):
-                param.grad *= self.alpha_gamma
-                param.grad += (1 - self.alpha_gamma)*self.grad_0th_order[lay]
+                param.grad *= 1
+                param.grad += 0*self.grad_0th_order[lay]
             self.time_report.end_timer("backward simulation")
 
             with torch.no_grad():
