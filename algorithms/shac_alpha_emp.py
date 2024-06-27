db0a31372936c01837a55ccd41844edcef8ad7c0
vstrozzi <vstrozzi@student.ethz.ch>
2024-06-27 16:19:23 +0200

    smaller sigma

--- algorithms/shac_alpha_emp.py
+++ algorithms/shac_alpha_emp.py
@@ -113,7 +113,7 @@ class SHAC_ALPHA_EMP:
             self.steps_num = self.env.episode_length
 
         # IMPL: smoothing noise
-        self.sigma = 0.000001 #cfg['params']['config'].get('sigma', 0.1)
+        self.sigma = 0.01 #cfg['params']['config'].get('sigma', 0.1)
         self.threshold_grad_norm_diff = 2
 
         # create actor critic network
