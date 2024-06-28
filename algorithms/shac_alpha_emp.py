fixed bug variance

algorithms/shac_alpha_emp.py
@@ -225,6 +225,9 @@ class SHAC_ALPHA_EMP:
 
             self.grad_0th_order[lay].fill_(0.)
 
+        self.grad_0th_order_std.fill_(0.)
+        self.grad_0th_order_std_scal = 0
+
 
         with torch.no_grad():
             if self.obs_rms is not None:
@@ -528,7 +531,9 @@ class SHAC_ALPHA_EMP:
             for lay in params.keys():   # init with 0 value
                 self.grad_1th_order_env[lay].fill_(0.)
                 self.grad_1th_order[lay].fill_(0.)
-            
+
+            self.grad_1th_order_std.fill_(0.)
+            self.grad_1th_order_std_scal = 0
 
             # Eval the 1th order gradient per environment and then batch it            
             for env in range(self.num_envs):